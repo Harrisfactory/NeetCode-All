bf9fb6f8ca1ffb6ed3759de79dbdf4bb1f7286e5
Harris Schrick <schrickh+github@gmail.com>
2023-11-11 14:07:32 -0800

    solved my way

--- reverse-linked-list.py
+++ reverse-linked-list.py
@@ -6,16 +6,33 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
+        #intuitive approach just stack and unstack O(n + n)
+        #if head is None:
+        #    return None
+
+        #stack: List[ListNode] = []
+
+        #cur = head
+
+        #while cur != None:
+        #    stack.append(cur.val)
+        #    cur = cur.next
+        
+        #new_head = ListNode(stack.pop())
+        #new_cur = new_head
+        #while len(stack) > 0:
+        #    new_cur.next = ListNode(stack.pop())
+        #    new_cur = new_cur.next
+            
+        #return new_head
+
+        #optimal approach
+
         prev = None
-        curr = head
-
-        while curr:
-            next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next
-
+        while head is not None:
+            nxt = head.next
+            head.next = prev
+            prev = head
+            head = nxt
+            
         return prev
-
-
-        #runtime is O(n)
