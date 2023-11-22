e9321a4bd762372197eef2dab1f6d5d232b03c25
Harris Schrick <schrickh+github@gmail.com>
2023-11-21 19:53:14 -0800

    solved with complexity

---
+++ search-in-a-binary-search-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        
+
+        #base case, does the node exist
+        if root is None:
+            return None
+        
+        if root.val > val:
+            return self.searchBST(root.left, val)
+        elif root.val < val:
+            return self.searchBST(root.right, val)
+        else:
+            #found, return subtree
+            return root
+        
+
+        #if balanced O(logn)
+        #if unbalanced O(h)
