37d5499ed16623684f9a8b1a0771c7ec306fc22d
Harris Schrick <schrickh+github@gmail.com>
2024-01-22 15:04:18 -0800

    better solution for longest common prefix

--- longest-common-prefix.py
+++ longest-common-prefix.py
@@ -1,22 +1,14 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
 
-        if len(strs) == 1 or strs[0] == "":
-            return strs[0]
+        blueprint: str = strs[0]
 
-        first_word = strs[0]
+        ctr: int = 0
 
-        cur_char = 0
-
-        result = ''
-
-        while cur_char < len(first_word):
-            for word in strs[1:len(strs)]:
-                if first_word[cur_char] and cur_char < len(word) and first_word[cur_char] == word[cur_char]:
-                    continue
-                else:
-                    return result
-            result += first_word[cur_char]
-            cur_char+=1
-            
-        return result
+        while ctr < len(blueprint):
+            for word in strs:
+                if ctr >= len(blueprint) or ctr >= len(word) or blueprint[ctr] != word[ctr]:
+                    return blueprint[:ctr]
+            ctr+=1
+        
+        return blueprint
