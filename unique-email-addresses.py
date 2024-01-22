a8ac872ed22ea09eb10f1f9c87b974b3f15ddda2
Harris Schrick <schrickh+github@gmail.com>
2024-01-22 15:27:58 -0800

    improved answer

--- unique-email-addresses.py
+++ unique-email-addresses.py
@@ -1,26 +1,19 @@
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
 
-        #we can ignore/strip out '.'
-        #we remove anything after + locally
 
-        fin_mails = {}
+        unique_emails = []
 
-        for i in range(len(emails)):
-
-            splimail = emails[i].split('@')
-
-            clean_mail = splimail[0].replace('.','')
-
-            clean_mail = clean_mail.partition('+')
-            
-            fin_mail = clean_mail[0]+ '@' + splimail[1]
-
-            if fin_mail not in fin_mails:
-                fin_mails[fin_mail] = 1
-            else:
-                continue
-
-
+        for email in emails:
+            email = email.split('@')
+            username = email[0]
+            domain = email[1]
+            #clean username
+            non_extended_username = username.split('+')[0]
+            non_extended_username = non_extended_username.replace('.','')
+            cleaned_email = non_extended_username + '@' + domain
+            if cleaned_email not in unique_emails:
+                unique_emails.append(cleaned_email)
         
-        return len(fin_mails)
+        return len(unique_emails)
+        
