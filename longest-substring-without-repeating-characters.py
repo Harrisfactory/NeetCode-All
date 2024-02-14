111578256fba40ebafb7d5f500d8a440ae27fa69
Harris Schrick <hschrick@Harriss-Laptop.local>
2024-02-13 16:28:51 -0800

    updated with better solution

--- longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters.py
@@ -1,26 +1,44 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
 
-        unique = set()
+        if len(s) < 1:
+            return 0
+        elif len(s) == 1:
+            return 1
+        
 
-        left = 0
-        right = 0
+        #sliding window: reason: because I see where I can snap and continue
 
-        max_longest = 0
+        #plan I can add repitions to a set and compare current count to a max count
 
-        while right < len(s):
-            if s[right] not in unique:
-                unique.add(s[right])
-                max_longest = max(max_longest, len(unique))
-                right+=1
-            elif s[right] in unique:
-                max_longest = max(max_longest, len(unique))
-                unique.remove(s[left])
-                left+=1
+        #If I repeat a char in this cycle I snap to that point and move end over one
 
+        #return max count
 
-        return max_longest
+        max_char_count: int = 0
 
-        #runtime is O(n) as each element in s is iterated through
+        non_repeated_chars = [s[0]]
 
-        #sliding window technique
+        start, end = 0, 1
+
+        cur_char_count: int = 1
+
+        while end < len(s):
+            #fail case
+            if s[end] in non_repeated_chars:
+                start+=1
+                #pop start
+                non_repeated_chars.pop(0)
+                max_char_count = max(max_char_count, cur_char_count)
+                cur_char_count-=1
+                continue
+             
+            non_repeated_chars.append(s[end])
+            end+=1
+            cur_char_count+=1
+            max_char_count = max(max_char_count, cur_char_count)
+            
+
+        return max_char_count
+
+        #O(n) I go through the elements once
